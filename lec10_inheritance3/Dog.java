5efc751a135d9b6432a6b6ae74a415f0b4b2ceb7
HaoLi <sjtuhl@gmail.com>
2021-07-19 11:00:40 +0800

    Done with lec8/9/10

--- lec10_inheritance3/Dog.java
+++ lec10_inheritance3/Dog.java
@@ -21,4 +21,14 @@ public class Dog implements Comparable<Dog> {
     public void bark() {
         System.out.println(name + " says: bark");
     }
+
+    private static class NameComparator implements Comparator<Dog>{
+        public int compare(Dog a,Dog b){
+            return a.name.compareTo(b.name);
+        }
+    }
+
+    public static Comparator<Dog> getNameComparator(){
+        return new NameComparator();
+    }
 }
